eb979180b93bbd568373331545bcf285eff98f8f
chrisvillegas <chris@clerkmann.com>
2021-09-05 17:19:23 -0500

    Arreglo de getters setter

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 use App\Domain\Entities\Registration;
 use App\Domain\ValueObjects\Cpf;
 use App\Domain\ValueObjects\Email;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 $registration = new Registration();
-$registration->setName('Chistopher flores')
+$registration->setName('Christopher')
     ->setBirthDate(new DateTimeImmutable('1978-02-14'))
     ->setEmail(new Email('cflores@innoceanmexico.com'))
     ->setRegistrationAt(new DateTimeImmutable())
     ->setRegistrationNumber(new Cpf('01234567890'));
 
-echo '<pre>'; print_r($registration);
+print_r($registration);
